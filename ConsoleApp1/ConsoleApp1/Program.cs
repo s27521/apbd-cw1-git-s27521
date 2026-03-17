d7973e9f40ce4f3d3caf5a84805135de49d7d4d1
Jarosław Trinh <s27521@pjwstk.edu.pl>
2026-03-18 00:37:17 +0100

    Created list of students

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,6 +11,30 @@ class Program
 {
     public static void Main(string[] args)
     {
-        
+        List<Student> studenci = new List<Student>();
+        studenci.Add(new Student
+        {
+            Id=1,
+            Name="John",
+            Wiek= 18
+        });
+        studenci.Add(new Student
+        {
+            Id=2,
+            Name="Annie",
+            Wiek= 22
+        });
+        studenci.Add(new Student
+        {
+            Id=3,
+            Name="Frank",
+            Wiek=30
+        });
+        studenci.Add(new Student
+        {
+            Id=4,
+            Name="Andrew",
+            Wiek=27
+        });
     }
 }
