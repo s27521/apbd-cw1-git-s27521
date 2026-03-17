612348e554b5fe29bd2fcae8c52cb047f02710b8
Jarosław Trinh <s27521@pjwstk.edu.pl>
2026-03-18 00:34:08 +0100

    Created initial Student class with fields

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Hello, World!");
+﻿public class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public List<double> oceny { get; set; }
+    public int Wiek { get; set; }
+}
